97baa19a8bf4da69b99a53cc87cd368bdff8c34f
Caitlin Schaefer <ceschae@cs.washington.edu>
2018-01-13 16:36:08 -0800

    major homepage work

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import constants from "./components/constants";
+import anonUser from './img/anon_user.png';
 
-import SignInView from "./components/SignIn";
 import SignUpView from "./components/SignUp";
 import Home from "./components/Home";
 import ProfileView from './components/Profile';
@@ -31,21 +31,71 @@ class App extends Component {
     this.authUnsub();
   }
 
+  handleSignIn(evt) {
+    evt.preventDefault();
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+    });
+  }
+
+  handleSignOut(evt) {
+    evt.preventDefault();
+    firebase.auth().signOut()
+      .catch(err => this.setState({errorMessage: err.message}));
+  }
+
+  getFirstName() {
+    return this.state.currUser.displayName.split(" ")[0];
+  }
+
   render() {
+    let imgStyle = {
+      'objectFit': 'cover',
+       height:'40px',
+       width:'40px'
+    }
     return (
       <Router> 
         <div className="App">
           <div className="jumbotron">
-            <h1 className="display-4">Welcome to Book Bot</h1>
-            <p className="lead">Your Very Own Tech Ghostwriter</p>
+            <div className="d-flex justify-content-center flex-row">
+              <div className="ml-auto mr-auto">
+                <h1 className="display-4">Welcome to Book Bot</h1>
+                <p className="lead">Your Very Own Tech Ghostwriter</p>
+              </div>
+              <div id="profile-img" className="align-self-center">
+              {
+                this.state.currUser ?
+                <div>
+                  <img src={this.state.currUser.photoURL} className="rounded" style={imgStyle} />
+                  <button type="button" class="btn btn-outline-primary" onClick={evt => this.handleSignOut(evt)}>Sign Out</button>
+                </div>
+                :
+                <div>
+                  <img src={anonUser} alt="Anonymous User" className="rounded" style={imgStyle} /> 
+                  <button type="button" class="btn btn-outline-primary" onClick={evt => this.handleSignIn(evt)}>Sign In</button>
+                </div>
+              }
+              </div>
+            </div>
             <hr className="my-4"/>
+            
           </div>
-          <Link to={constants.routes.profile} className="nav-link">Profile</Link>
           <div className="container">
             <div className="row">
            
               <Switch>
-                <Route exact path={constants.routes.signin} component={SignInView} />
                 <Route path={constants.routes.signup} component={SignUpView} />
                 <Route path={constants.routes.about} component={Home} />
                 <Route component={Home} />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,48 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className="container">
-                <p>Still in construction, please be patient!</p> 
+                <nav id="home-nav" className="navbar navbar-light bg-light justify-content-between">
+                    <a className="navbar-brand" href="#">Learn about what we do:</a>
+                    <ul className="navbar-nav d-flex flex-row">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#overview">Overview</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#how">How it works</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contribute">Contribute</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#generate">Generate</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#about">About Us</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div data-spy="scroll" data-target="#home-nav" data-offset="0" className="d-flex flex-column">
+                    <div id="overview" className="d-flex flex-column">
+                        <h4 className="align-self-start">Overview</h4>
+                        <p align="left">Give us some content, tell us what it's like. Or, tell us what you want, and we'll give you some content. It's just that easy.</p>
+                    </div>
+                    <div id="how" className="d-flex flex-column">
+                        <h4 className="align-self-start">How it works</h4>
+                        <p align="left">Phasellus mattis metus dui, ac accumsan arcu bibendum et. Ut facilisis mattis scelerisque. Proin ac lorem ac ex ultrices malesuada sed non sem. Phasellus diam ipsum, dignissim at aliquet id, bibendum vel sapien. Duis ullamcorper libero convallis, pellentesque nunc quis, dictum tortor. Pellentesque id nulla id ante ultricies venenatis. Morbi mattis leo at tellus ultricies feugiat non quis massa. Mauris consectetur, felis id rutrum tristique, mi arcu ultrices tellus, et elementum libero ex at neque. Sed ullamcorper cursus purus, eu egestas ligula convallis eu. Sed molestie ante a sem malesuada, ut tempor quam varius. Vivamus dictum quam tortor, ac bibendum mauris ultrices vitae.</p>
+                    </div>
+                    <div id="contribute" className="d-flex flex-column">
+                        <h4 className="align-self-start">Contribute</h4>
+                        <p align="left">Aliquam ac sem sit amet velit porttitor vulputate scelerisque vel orci. Sed mi risus, molestie nec lectus scelerisque, egestas euismod lacus. Phasellus ac pulvinar sem. Morbi porttitor fermentum justo. Curabitur massa risus, imperdiet id libero eget, lacinia pulvinar lectus. Ut finibus metus non lectus dictum, vitae mattis ipsum tincidunt. Integer a bibendum purus. In vulputate nisi nisi, ac ultricies tortor iaculis vitae. Donec suscipit nunc in nulla pellentesque, ac eleifend eros varius. Nullam sodales tellus sit amet sapien dignissim pellentesque. Nunc a convallis libero. In volutpat tempor sapien vel pulvinar. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas eget accumsan nulla. Donec fringilla blandit feugiat. Cras pretium justo vel risus bibendum euismod vitae eget justo.</p>
+                    </div>
+                    <div id="generate" className="d-flex flex-column">
+                        <h4 className="align-self-start">Generate</h4>
+                        <p align="left">Fusce gravida magna a maximus aliquam. Quisque at venenatis odio. Nullam at sagittis magna, ac pulvinar tortor. Aenean laoreet nunc nec ex tempor, nec fringilla urna malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi in magna nec est porta ornare nec sed augue. Maecenas gravida porttitor nisl, ut hendrerit velit egestas a. Etiam luctus, velit eget congue rutrum, sapien leo imperdiet mauris, eget dapibus lacus mi ut purus. Aliquam sagittis dapibus velit, vel interdum lacus mollis nec. Aenean ac ex congue, fermentum est et, molestie massa. Ut condimentum, arcu et rutrum iaculis, arcu velit varius orci, id lobortis augue tellus nec urna. Aenean ut lacus consequat mi tristique viverra. Etiam sed libero sit amet leo vehicula dignissim id non nibh. Curabitur gravida viverra metus quis tempor. Phasellus venenatis nulla nec euismod tristique. Nulla sollicitudin orci nec dolor vulputate ornare.</p>
+                    </div>
+                    <div id="about" className="d-flex flex-column">
+                        <h4 className="align-self-start">About Us</h4>
+                        <p align="left">Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean vitae pulvinar nisl, a mattis erat. Aenean neque lectus, fermentum non porta sed, lacinia vitae leo. Donec et tincidunt nulla. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vehicula eget eros a interdum. Fusce varius sit amet sem non porta. Maecenas convallis mauris enim, nec tincidunt risus ultrices id. Nunc ultricies, metus at lacinia porta, orci odio congue orci, nec scelerisque arcu nulla et tortor. Morbi tincidunt volutpat metus. Vestibulum mollis vulputate mauris, non dignissim sapien cursus at. Nulla ut justo vel tortor gravida ultrices a et eros. Ut scelerisque lacinia nunc. Aenean libero purus, consequat ac posuere in, gravida eu nibh. Fusce non iaculis nisl.</p>
+                    </div>
+                </div>
             </div>
         )
     }
